73af8a19e23e6c289aa9d34aab36c81d06e647d2
sxyazi <sxyazi@gmail.com>
2017-10-03 23:11:25 +0800

    修改Config结构

--- types/config.go
+++ types/config.go
@@ -1,16 +1,12 @@
 package types
 
-type Db struct {
-	Host     string `json:"host"`
-	Port     int    `json:"port"`
-	Database string `json:"database"`
-}
-
-type App struct {
-	Debug bool `json:"debug"`
-}
-
 type Config struct {
-	Db  Db  `json:"db"`
-	App App `json:"app"`
+	Db struct {
+		Host     string
+		Port     int
+		Database string
+	}
+	App struct {
+		Debug bool
+	}
 }
