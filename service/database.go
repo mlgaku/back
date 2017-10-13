e1c8b9a21bbaead21331ec6b55d9ea98f632ccb8
sxyazi <sxyazi@gmail.com>
2017-10-14 03:20:18 +0800

    删除 FindId 方法

--- service/database.go
+++ service/database.go
@@ -3,7 +3,6 @@ package service
 import (
 	"fmt"
 	"gopkg.in/mgo.v2"
-	"gopkg.in/mgo.v2/bson"
 )
 
 type Database struct {
@@ -24,11 +23,6 @@ func (d *Database) connect() {
 // 获得 Collection 实例
 func (d *Database) C(name string) *mgo.Collection {
 	return d.Session.DB(APP.Conf.Db.Database).C(name)
-}
-
-// 通过 ID 查询
-func (d *Database) FindId(tab, id string) *mgo.Query {
-	return d.C(tab).FindId(bson.ObjectIdHex(id))
 }
 
 // 获得 Database 实例
