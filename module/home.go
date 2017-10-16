257aa93254264a0d2332126e87aefcf7a3e63a65
sxyazi <sxyazi@gmail.com>
2017-10-16 21:14:17 +0800

    删除测试代码

--- module/home.go
+++ module/home.go
@@ -1,19 +1,3 @@
 package module
 
-import (
-	"fmt"
-	. "github.com/mlgaku/back/service"
-	. "github.com/mlgaku/back/types"
-	"math/rand"
-)
-
 type Home struct{}
-
-// 测试
-func (*Home) Hello(req *Request, res *Response) Value {
-	for i := 0; i < 10; i++ {
-		res.Write([]byte("ddd"))
-	}
-
-	return fmt.Sprintf("[%g] hello world", rand.Float64())
-}
